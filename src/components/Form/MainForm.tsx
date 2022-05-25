300366661b4f78cdf99800e5504afa96e2c97e2c
Stanislav Klymoshenko <s.klymoshenko@gmail.com>
2022-05-25 20:33:53 +0200

    add file upload validations

--- src/components/Form/MainForm.tsx
+++ src/components/Form/MainForm.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Input, Button, InputNumber } from 'antd';
+import { Input, Button, InputNumber, message } from 'antd';
 import "./MainForm.css";
-import FileUpload from "../FileUpload/FileUpload";
 import { Field, reduxForm, InjectedFormProps, WrappedFieldProps, WrappedFieldMetaProps } from 'redux-form'
+import { acceptOnlyNumber, maxFileSize, numberRange, required } from "./validations";
 
 
 const errorMessage = (meta: WrappedFieldMetaProps): JSX.Element | null => {
-  return (meta.error && meta.touched ? <span className="error-message">{meta.error}</span> : null)
+  return (meta && meta.invalid && meta.error && meta.touched && <span className="error-message">{meta.error}</span>)
 }
 
 // Custom inputs for styling purposes
@@ -28,38 +28,26 @@ const renderHeightInput = (props: WrappedFieldProps & WrappedFieldMetaProps): JS
 
 const renderFileUpload = (props: WrappedFieldProps & WrappedFieldMetaProps): JSX.Element => {
   return (<div>
-    <FileUpload />
-    {errorMessage(props.meta)}
+    <input type="file" onChange={(e) => {
+      e.preventDefault()
+      props.input.onChange(e.target.files![0].name)
+    }}/>
   </div>
   )
 }
 
-// values is actually typed as any o_0
-// https://github.com/DefinitelyTyped/DefinitelyTyped/blob/9ce52af612e29ff0bac4317bde78d0acab29afdb/types/redux-form/v6/lib/Form.d.ts#L5
-const onSubmit = (values: any) => {
-  console.log(values);
-}
+  // values is actually typed as any o_0
+  // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/9ce52af612e29ff0bac4317bde78d0acab29afdb/types/redux-form/v6/lib/Form.d.ts#L5
+  const onSubmit = (values: any) => {
+    const fileInput: HTMLInputElement | null = document.querySelector('input[type=file]')
+    const file = fileInput?.files?.[0]
 
-// Validations
-const required = (v: any): string | undefined => {
-  if (!v) return 'This field is required'
-
-  return undefined
-}
-
-const acceptOnlyNumber = (v: any): string | undefined => {
-  if (isNaN(v)) return 'Value should be a valid number'
-
-  return undefined
-}
-
-
-const numberRange = (v: any): string | undefined => {
-  if (v && v > 500) return 'Value cant be more than 500'
-  if (v && v < 0) return 'Value cant be less than 0'
-
-  return undefined
-}
+    const notValidFile = maxFileSize(file)
+    if (notValidFile) {
+      message.error(notValidFile)
+      return
+    }
+  }
 
 const MainForm: React.FC = (props) => {
   const { handleSubmit, valid } = props as InjectedFormProps
@@ -79,7 +67,7 @@ const MainForm: React.FC = (props) => {
             </div>
           </div>
           <div className="main-form-file-upload">
-          <Field name="name" component={renderFileUpload} type="file"/>
+          <Field name="file" component={renderFileUpload} type="file" validate={[required]}/>
           </div>
         </div>
         <Button type="primary" block htmlType="submit" disabled={!valid}>
@@ -91,18 +79,6 @@ const MainForm: React.FC = (props) => {
 }
 
 export default reduxForm({
-  // a unique name for the form
   form: 'filedata',
-  onSubmit
+  onSubmit,
 })(MainForm)
-
-// const MainForm: React.FC = () => {
-//   const [name, setName] = useState("")
-//   const [height, setHeight] = useState(0)
-
-
-
-  // return (
-
-  // );
-// }
